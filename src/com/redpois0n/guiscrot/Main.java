e8949a7840d1601f8e423821acfc7972691b100f
redpois0n <red@cmail.nu>
2015-05-03 14:11:09 +0200

    Sets default system theme, disables JNativeHook logging

--- src/com/redpois0n/guiscrot/Main.java
+++ src/com/redpois0n/guiscrot/Main.java
@@ -1,16 +1,33 @@
 package com.redpois0n.guiscrot;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.UIManager;
+
 import org.jnativehook.GlobalScreen;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			//GlobalScreen.registerNativeHook();
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			registerNativeHook();
 			
 			new RegionFrame().setVisible(true);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Disable JNativeHook logging and enable
+	 * @throws Exception
+	 */
+	private static void registerNativeHook() throws Exception {
+		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+		logger.setLevel(Level.OFF);
+		
+		GlobalScreen.registerNativeHook();
 	}
 }
