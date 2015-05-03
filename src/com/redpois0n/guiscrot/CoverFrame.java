a6c907afd23b12a768043fe9cc7f37b1dd87a0e0
redpois0n <red@cmail.nu>
2015-05-03 15:46:46 +0200

    Works on multiple monitors, draws arrow in correct frame and place

--- src/com/redpois0n/guiscrot/CoverFrame.java
+++ src/com/redpois0n/guiscrot/CoverFrame.java
@@ -7,12 +7,23 @@ import java.awt.Rectangle;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.mouse.NativeMouseEvent;
+import org.jnativehook.mouse.NativeMouseMotionListener;
+
 @SuppressWarnings("serial")
-public class CoverFrame extends JFrame {
+public class CoverFrame extends JFrame implements NativeMouseMotionListener {
 	
 	public static final float OPACITY = 0.5F;
 	
+	private Rectangle rect;
+	
+	private int x;
+	private int y;
+	
 	public CoverFrame(Rectangle rect) {
+		this.rect = rect;
+		GlobalScreen.addNativeMouseMotionListener(this);
 		setUndecorated(true);
 		setBounds(rect);
 		setContentPane(new CoverPanel());
@@ -27,7 +38,24 @@ public class CoverFrame extends JFrame {
 			
 			g.setColor(Color.black);
 			g.fillRect(0, 0, getWidth(), getHeight());
+			
+			g.setColor(Color.white);
+			g.fillRect(x, 0, 1, getHeight());
+			g.fillRect(0, y, getWidth(), 1);
 		}
+	}
+
+	@Override
+	public void nativeMouseDragged(NativeMouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void nativeMouseMoved(NativeMouseEvent arg0) {
+		x = arg0.getX() - rect.x;
+		y = arg0.getY() - rect.y;
+
+		repaint();
 	}
 
 }
