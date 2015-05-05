5d90f57e3696181bfb8341e414dd3f2ad6cd2c9e
redpois0n <red@cmail.nu>
2015-05-05 21:33:10 +0200

    Logger class

---
+++ src/com/redpois0n/gscrot/Logger.java
@@ -0,0 +1,21 @@
+package com.redpois0n.gscrot;
+
+import java.util.Date;
+
+@SuppressWarnings("deprecation")
+public class Logger {
+
+	public static void log(Object obj) {
+		Date date = new Date(System.currentTimeMillis());
+		
+		String hours = (date.getHours() + "").length() == 1 ? "0" + date.getHours() : Integer.toString(date.getHours());
+		String minutes = (date.getMinutes() + "").length() == 1 ? "0" + date.getMinutes() : Integer.toString(date.getMinutes());
+		String seconds = (date.getSeconds() + "").length() == 1 ? "0" + date.getSeconds() : Integer.toString(date.getSeconds());
+		
+		String time = hours + ":" + minutes + ":" + seconds;
+		
+		String s = "[" + time + "] [LOG]: " + obj.toString();
+		
+		System.out.println(s);
+	}
+}
