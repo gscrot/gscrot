1140d781a60dd43539f694fbccf31d417db941d2
redpois0n <red@cmail.nu>
2015-05-05 20:24:31 +0200

    Config

---
+++ src/com/redpois0n/gscrot/Config.java
@@ -0,0 +1,53 @@
+package com.redpois0n.gscrot;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Config {
+	
+	public static final Map<String, String> CONFIG = new HashMap<String, String>();
+	public static final File CONFIG_FILE = new File(".gscrot");
+	
+	public static void load() throws Exception {
+		CONFIG.clear();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(CONFIG_FILE)));
+		
+		String line;
+		
+		while ((line = reader.readLine()) != null) {
+			if (!line.startsWith("#") && line.contains("=")) {
+				String k = line.substring(0, line.indexOf("="));
+				String v = line.substring(line.indexOf("="), line.length());
+				
+				System.out.println(k + ", " + v);
+				
+				CONFIG.put(k, v);
+			}
+		}
+		
+		reader.close();
+	}
+	
+	public static void save() throws Exception {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(CONFIG_FILE)));
+		
+		writer.write("# gscrot generated config");
+		writer.newLine();
+		
+		for (String k : CONFIG.keySet()) {
+			writer.write(k + "=" + CONFIG.get(k));
+			writer.newLine();
+		}
+		
+		writer.close();
+	}
+
+}
