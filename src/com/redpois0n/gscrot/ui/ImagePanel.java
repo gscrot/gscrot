8cc6aed674c52f66891d00b4eb9364068a0f4291
redpois0n <red@cmail.nu>
2015-05-05 22:55:07 +0200

    draws grid

--- src/com/redpois0n/gscrot/ui/ImagePanel.java
+++ src/com/redpois0n/gscrot/ui/ImagePanel.java
@@ -1,5 +1,6 @@
 package com.redpois0n.gscrot.ui;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
@@ -7,6 +8,8 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class ImagePanel extends JPanel {
+	
+	public static final Color COLOR_GRAY = new Color(211, 211, 211);
 	
 	private BufferedImage image;
 
@@ -18,12 +21,23 @@ public class ImagePanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
+		int width = getWidth();
+		int height = getHeight();
+				
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				if ((x + y) % 2 == 1) {
+					g.setColor(COLOR_GRAY);
+				} else {
+					g.setColor(Color.white);
+				}
+				
+				g.fillRect(x * 10, y * 10, 10, 10);
+			}
+		}
+		
 		if (image != null) {
 			g.drawImage(image, getWidth() / 2 - image.getWidth() / 2, getHeight() / 2 - image.getHeight() / 2, image.getWidth(), image.getHeight(), null);
-			
-			if (getWidth() > image.getWidth()) {
-				
-			}
 		}
 	}
 	
