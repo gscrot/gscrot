782fd849a320509a455b5008147801e4b531f83f
redpois0n <red@cmail.nu>
2015-05-05 21:23:26 +0200

    Uses BoxLayout instead of GridLayout, aligns properly and next to eachother

--- src/com/redpois0n/gscrot/ui/MenuPanel.java
+++ src/com/redpois0n/gscrot/ui/MenuPanel.java
@@ -1,36 +1,27 @@
 package com.redpois0n.gscrot.ui;
 
 import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 
 @SuppressWarnings("serial")
 public class MenuPanel extends JPanel {
-	
+
 	public MenuPanel() {
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 0, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
-		setLayout(gridBagLayout);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 	}
 
-	public void addButton(JButton btn) {
-		GridBagConstraints grid = new GridBagConstraints();
-		grid.gridx = 0;
-		grid.gridy = getComponentCount();
-		grid.anchor = GridBagConstraints.WEST;
-		
+	public void addButton(final JButton btn) {
 		JToolBar bar = new JToolBar();
+
 		bar.setFloatable(false);
 		bar.add(btn);
-		
-		add(bar, grid);
+
+		bar.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+		add(bar);
 	}
 }
