0170f6f0ff915f1a45bed50779a8a7aec1bc4337
redpois0n <red@cmail.nu>
2015-05-05 13:38:40 +0200

    Converts JMenuItem to JButton, and converts JMenu to JButton with PopupMenu

--- src/com/redpois0n/guiscrot/MainFrame.java
+++ src/com/redpois0n/guiscrot/MainFrame.java
@@ -2,8 +2,9 @@ package com.redpois0n.guiscrot;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,48 +15,49 @@ import javax.swing.JPopupMenu;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
-	
+
 	public static final MainFrame INSTANCE = new MainFrame();
 
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		add(panel, BorderLayout.WEST);
-		
+
 		JPopupMenu global = GlobalPopupMenu.getPopupMenu();
-		
+
 		for (Component c : global.getComponents()) {
-			if (c instanceof JMenuItem) {
+			if (c instanceof JMenu) {
+				JMenu m = (JMenu) c;
+
+				final JButton btn = new JButton(m.getText(), m.getIcon());
+				panel.add(btn);
+
+				final JPopupMenu menu2 = new JPopupMenu();
+
+				btn.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+						menu2.show((Component) e.getSource(), btn.getWidth(), 0);
+						menu2.requestFocus();
+					}
+				});
+
+				for (Component c1 : m.getMenuComponents()) {
+					menu2.add(c1);
+				}
+			} else if (c instanceof JMenuItem) {
 				JMenuItem mi = (JMenuItem) c;
 				JButton btn = new JButton(mi.getText(), mi.getIcon());
 				panel.add(btn);
-				
+
 				for (ActionListener l : mi.getActionListeners()) {
 					btn.addActionListener(l);
 				}
-			} else if (c instanceof JMenu) {
-				JMenu m = (JMenu) c;
-				
-				final JButton btn = new JButton(m.getText(), m.getIcon());
-				panel.add(btn);
-				
-				final JPopupMenu menu2 = new JPopupMenu();
-				
-				btn.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						menu2.show(null, btn.getX() + btn.getWidth(), 0);
-					}
-				});
-				
-				for (Component c1 : m.getComponents()) {
-					menu2.add(c1);
-				}
 			}
 		}
-		
+
 	}
 
 }
