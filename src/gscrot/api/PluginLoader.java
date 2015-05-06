59df86472128416fc83e4bfdcce8712c9179601b
redpois0n <red@cmail.nu>
2015-05-06 22:16:01 +0200

    Doesn't crash if plugin dir doesn't exist or is empty

--- src/gscrot/api/PluginLoader.java
+++ src/gscrot/api/PluginLoader.java
@@ -6,18 +6,31 @@ import java.util.List;
 
 import pluginlib.Plugin;
 
+import com.redpois0n.gscrot.Logger;
+
 public class PluginLoader {
 	
 	public static List<Plugin> plugins = new ArrayList<Plugin>();
 	
 	public static void loadPlugins() throws Exception {
-		for (File file : new File("plugins/").listFiles()) {
-			Plugin plugin = new Plugin(file);
-			
-			plugin.load();
-			
-			plugins.add(plugin);
+		File dir = new File("plugins/");
+		File files[] = dir.listFiles();
+		
+		if (files != null) {
+			for (File file : files) {
+				load(file);
+			}
 		}
+	}
+	
+	public static void load(File file) throws Exception {
+		Logger.log("Loading plugin " + file.getName());
+		
+		Plugin plugin = new Plugin(file);
+		
+		plugin.load();
+		
+		plugins.add(plugin);
 	}
 
 }
