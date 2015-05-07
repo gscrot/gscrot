587d38597c4bb4146c7d1bdfc74877adcea2bf11
redpois0n <red@cmail.nu>
2015-05-07 17:24:27 +0200

    generic

--- src/gscrot/api/PluginLoader.java
+++ src/gscrot/api/PluginLoader.java
@@ -10,7 +10,7 @@ import com.redpois0n.gscrot.Logger;
 
 public class PluginLoader {
 	
-	public static List<Plugin> plugins = new ArrayList<Plugin>();
+	public static List<Plugin<gscrot.api.Plugin>> plugins = new ArrayList<Plugin<gscrot.api.Plugin>>();
 	
 	public static void loadPlugins() throws Exception {
 		File dir = new File("plugins/");
@@ -26,7 +26,7 @@ public class PluginLoader {
 	public static void load(File file) throws Exception {
 		Logger.log("Loading plugin " + file.getName());
 		
-		Plugin plugin = new Plugin(file);
+		Plugin<gscrot.api.Plugin> plugin = new Plugin<gscrot.api.Plugin>(file);
 		
 		plugin.load();
 		
