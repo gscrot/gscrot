545ae692d5c58a7957ffc9c567825417d6d7110f
redpois0n <red@cmail.nu>
2015-05-13 13:09:03 +0200

    Implement ClipboardHelper

--- src/com/redpois0n/gscrot/ClipboardHelper.java
+++ src/com/redpois0n/gscrot/ClipboardHelper.java
@@ -1,9 +1,15 @@
 package com.redpois0n.gscrot;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
 public class ClipboardHelper {
 	
 	public static void set(String s) {
-		
+		StringSelection selection = new StringSelection(s);
+	    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+	    clipboard.setContents(selection, selection);
 	}
 
 }
