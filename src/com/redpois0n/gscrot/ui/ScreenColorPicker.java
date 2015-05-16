92c2506bea1dd632ac3b4664e91b4c9021b7bb95
redpois0n <red@cmail.nu>
2015-05-17 00:35:26 +0200

    Overrides methods that causes dragging, gets selected color on click

--- src/com/redpois0n/gscrot/ui/ScreenColorPicker.java
+++ src/com/redpois0n/gscrot/ui/ScreenColorPicker.java
@@ -1,19 +1,47 @@
 package com.redpois0n.gscrot.ui;
 
-import java.awt.Image;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 @SuppressWarnings("serial")
 public class ScreenColorPicker extends RegionCapture {
+	
+	private Color selected = null;
 
-	public ScreenColorPicker(Rectangle rect, Image image) {
+	public ScreenColorPicker(Rectangle rect, BufferedImage image) {
 		super(rect, image);
 	}
 	
 	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		if (selected != null) {
+			g.setColor(selected);
+			// draw preview
+		}
+	}
+	
+	@Override
 	public void mousePressed(MouseEvent arg0) {
-		System.out.println("clik");
+		int array[] = new int[1];
+		image.getRGB(x, y, 1, 1, array, 0, 1);
+
+		selected = new Color(array[0]);
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+
 	}
 
 }
