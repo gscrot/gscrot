0f3ccefe2907ef90b80b382d355305b1e965bab9
redpois0n <red@cmail.nu>
2015-06-06 17:26:09 +0200

    JDropDownButton

---
+++ src/com/redpois0n/gscrot/ui/JDropDownButton.java
@@ -0,0 +1,34 @@
+package com.redpois0n.gscrot.ui;
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPopupMenu;
+
+@SuppressWarnings("serial")
+public class JDropDownButton extends JButton {
+	
+	private JPopupMenu menu;
+
+	public JDropDownButton(String text) {
+		this(text, null);
+	}
+	
+	public JDropDownButton(String text, ImageIcon icon) {
+		super(text, icon);
+		
+		addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				menu.show((Component) e.getSource(), getWidth(), 0);
+				menu.requestFocus();
+			}
+		});
+	}
+	
+	public JPopupMenu getMenu() {
+		return this.menu;
+	}
+}
